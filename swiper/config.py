a0f0e1e0f0cff3cb32f005713bf10556ed32d21b
a1570 <5613720>
2019-04-10 20:31:44 +0800

    Default Changelist

--- swiper/config.py
+++ swiper/config.py
@@ -1,12 +1,12 @@
 YZX_SMS_URL='https://open.ucpaas.com/ol/sms/sendsms'
 
 YZX_SMS_PARAMS = {
-     "sid":"bd316ba76ed4bd8d807abcf06129a0c9",
-     "token":"c50d997539a0992a1c8a99b99aeb1f62",
-     "appid":"0396ca9d5f9349eaa76eb6cb2231d372",
-     "templateid":"445793",
-     "param":None,
-     "mobile":None,
+    "sid":"adf30817cc6eb2f264cc787ca43bbaf4",
+    "token":"8c23184c90f9cd032715378bd815c660",
+    "appid":"0974ad7442ea4c6d89de2d26decc5296",
+    "templateid":"453692",
+    "param":None,
+    "mobile":None,
 }
 # 需要填写你的 Access Key 和 Secret Key
 QINIU_ACCESS_KEY = 'omsZgpencslXzRSRDnVD3lvA6gBt0I_KdSUEwWhQ'
