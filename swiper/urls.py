cd372891a5f00eb1140d60b246645603b62d5251
a1570 <5613720>
2019-04-11 11:47:19 +0800

    Default Changelist

--- swiper/urls.py
+++ swiper/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from user import api as user_api
+from social import api as social_api
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +25,12 @@ urlpatterns = [
     url(r'usr/api/get_profile', user_api.get_profile),
     url(r'usr/api/set_profile', user_api.set_profile),
     url(r'usr/api/upload_avatar', user_api.upload_avatar),
+
+    url(r'social/api/get_rcmd_user', social_api.get_rcmd_user),
+    url(r'social/api/like', social_api.like),
+    url(r'social/api/superlike', social_api.superlike),
+    url(r'social/api/dislike', social_api.dislike),
+    url(r'social/api/regret', social_api.regret),
+    url(r'social/api/get_friends', social_api.get_friends),
+    url(r'social/api/get_friend_info', social_api.get_friend_info),
 ]
